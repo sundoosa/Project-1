75f147d910a140eb19f4e39d157daa0c3ba5a78c
sundoosa <89888481+sundoosa@users.noreply.github.com>
2022-09-10 00:55:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-sw5qxn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-sw5qxn)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+document.querySelector("button").addEventListener("click", function(){
+  alert('Welcome!');
+})
